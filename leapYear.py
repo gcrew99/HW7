72ae96afde3dbcba0a629a93e04b05e1eff187a4
gcrew99 <61959926+gcrew99@users.noreply.github.com>
2021-03-07 22:54:40 -0800

    Added code to ensure that the first test succeeds by checking whether the inputted number is divisible by four, then returning "Leap Year"

--- leapYear.py
+++ leapYear.py
@@ -1,5 +1,12 @@
 # Written by Gabriel Crew
 # Function to determine whether a given year is a leap  year
 
+leapStr = input("Enter a Year and I'll tell you if it's a leap year or not:" )
+
 def leapYear(leapStr):
- pass
+  leapNum = int(leapStr)
+  if (leapNum % 4) == 0:
+    string = "Leap year"
+  else:
+    string ="Not a Leap Year"
+  return string
