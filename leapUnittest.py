866cd5edf4f7aaa83de35620ac0b12cbcea949db
gcrew99 <61959926+gcrew99@users.noreply.github.com>
2021-03-07 23:03:34 -0800

    Added the third test that checks that numbers evenly divisible by 4,100, and 400 are a "Leap Year"

--- leapUnittest.py
+++ leapUnittest.py
@@ -5,6 +5,7 @@ import leapYear
 class TestCase(unittest.TestCase):
   def test1(self): self.assertEqual(leapYear(4),"Leap Year")
   def test2(self): self.assertEqual(leapYear(200),"Not Leap Year")
+  def test3(self): self.assertEqual(leapYear(400),"Not Leap Year")
 
 if __name__ == '__main__':
   unittest.main()
