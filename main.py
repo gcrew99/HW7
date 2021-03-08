0586b8504303f914f6073287b041b93427b88260
gcrew99 <61959926+gcrew99@users.noreply.github.com>
2021-03-07 22:01:10 -0800

    Initial commit with first test insuring "Fizz" functionality

---
+++ main.py
@@ -0,0 +1,11 @@
+#Gabriel Crew
+import unittest 
+import code
+
+class TestCase(unittest.TestCase):
+  def test1(self): self.assertEqual(fizzBuzz(3),"1,2,Fizz")
+
+
+
+if __name__ == '__main__':
+  unittest.main()
