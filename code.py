2d59b8d8fb980197f01b6b5a8db93bc082783d63
gcrew99 <61959926+gcrew99@users.noreply.github.com>
2021-03-07 22:06:44 -0800

    Added code to ensure that the first test passes whenever a multiple of 3 is found

--- code.py
+++ code.py
@@ -1,4 +1,14 @@
 
 
-def fizzBuzz():
-  pass
+def fizzBuzz(x):
+      string = ""
+      for i in range(x):
+        if (i%3 == 0): 
+          string = string + "Fizz"
+        else:
+          string = string + i
+      return string
+
+x_in = input("Type number from 1 to 100  ")
+x_in = int(x_in)
+fizzBuzz(x_in)
