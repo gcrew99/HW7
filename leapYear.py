f8afc487ed968bd83e29dd3dc03707d9ae87ff4c
gcrew99 <61959926+gcrew99@users.noreply.github.com>
2021-03-07 23:01:36 -0800

    Added code to ensure that the inputting a number like 200 that is divisible by both 100 and 4 returns "Not Leap Year"

--- leapYear.py
+++ leapYear.py
@@ -6,7 +6,10 @@ leapStr = input("Enter a Year and I'll tell you if it's a leap year or not:" )
 def leapYear(leapStr):
   leapNum = int(leapStr)
   if (leapNum % 4) == 0:
-    string = "Leap year"
+    if(leapNum%100) == 0:
+      string ="Not a Leap Year"
+    else:
+      string ="Leap Year"
   else:
     string ="Not a Leap Year"
   return string
